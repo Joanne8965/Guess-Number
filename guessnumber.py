d5c4927a2ce4f4de10d324ee7d4d494362b128c5
Joanne Chou <joannechou89@gmail.com>
2019-10-18 11:11:34 +0200

    add count

--- guessnumber.py
+++ guessnumber.py
@@ -7,16 +7,19 @@
 import random
 
 r = random.randint(1, 100)  #隨機整數, r = 答案
-
+count = 0
 
 while True:                 #無限巡迴
+    count += 1              #count = count + 1
     num = input('請猜數字從1到100 Guess a number between 1 and 100:')
     num = int(num)
     if num == r:
         print('你猜中了！You are right!')
+        print('這是你猜的第', count, '次','You have tried', count, 'times')
         break
     elif num > r:
         print('比答案大 Too big!')
     elif num < r:
         print('比答案小 Too small!')
+    print('這是你猜的第', count, '次','You have tried', count, 'times')
 
